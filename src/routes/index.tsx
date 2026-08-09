b1f7ea4a0f032c26cc50fedc05857b92914f347a
worldischanging441-wq <worldischanging441@gmail.com>
2026-08-09 14:11:02 +0100

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -19,7 +19,7 @@ import { ChartBackdrop } from "@/components/site/Chart";
 import { chapters, faqs, testimonials } from "@/components/site/data";
 import shot1 from "@/assets/reader-01-before.jpg";
 reader-01-after.jpg
-import shot3 from "@/assets/student-6032595525506371666.jpg.asset.json";
+reader-02.jpg
 import shot4 from "@/assets/student-6032595525506371665.jpg.asset.json";
 import shot5 from "@/assets/student-6032595525506371663.jpg.asset.json";
 import shot6 from "@/assets/student-6032595525506371662.jpg.asset.json";
